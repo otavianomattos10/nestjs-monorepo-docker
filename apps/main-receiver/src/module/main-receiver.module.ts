fd359d9de4a3a24463cebce6578d6ae0ebdef796
Teste <otavianomattos10@gmail.com>
2022-09-22 16:52:26 -0400

    feat: created grpc client

--- apps/main-receiver/src/module/main-receiver.module.ts
+++ apps/main-receiver/src/module/main-receiver.module.ts
@@ -1,3 +1,4 @@
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { Module, Logger } from '@nestjs/common';
 import {
   PrometheusModule,
@@ -5,10 +6,25 @@ import {
 } from '@willsoto/nestjs-prometheus';
 import { MainReceiverController } from '../controller/main-receiver.controller';
 import { MainReceiverService } from '../services/main-receiver.service';
+import { join } from 'path';
+import { MainReceiverGrpcClientController } from '../grpc-client/main-receiver-grpc.controller';
 
 @Module({
-  imports: [PrometheusModule.register()],
-  controllers: [MainReceiverController],
+  imports: [
+    PrometheusModule.register(),
+    ClientsModule.register([
+      {
+        name: 'FIRSTCONSUMER_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          url: 'first_consumer:50051',
+          package: 'firstConsumer',
+          protoPath: join(__dirname, '../proto/first-consumer.proto'),
+        },
+      },
+    ]),
+  ],
+  controllers: [MainReceiverController, MainReceiverGrpcClientController],
   providers: [
     MainReceiverService,
     Logger,
